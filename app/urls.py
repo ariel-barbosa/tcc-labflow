d4bf25cf94bd1a95e7f8aa1d92b4208ab03ab4a8
Ariel Santos <ariel.santos@docemineiro.ind>
2025-04-08 21:48:13 -0300

    commit

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # login, cadastro e logOut no sistema
     path('', views.login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('esqueci-senha/', views.esqueci_senha, name='esqueci_senha'),
